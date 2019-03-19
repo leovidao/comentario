6864cf5bfb9f6dac36f567103427ac4bc3fe95da
leovidao <leovidao@gmail.com>
2019-03-18 21:43:08 -0300

    aula 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  state = {
+    newComment: '',
+    comments: [
+      'Comment 1',
+      'Comment 2',
+      'Comment 3'
+    ]
+  }
+  sendComment = () => {
+    this.setState({
+      comments: [...this.state.comments, this.state.newComment],
+      newComment: ''
+    })
+  } 
+  handleChange = event => {
+    this.setState({
+      newComment: event.target.value
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        {/* NewComment */}
+        <div>
+          <textarea value={this.state.newComment} onChange={this.handleChange}></textarea>
+          <button onClick={this.sendComment}>Enviar</button>
+        </div>
+        {/* Comments */}
+        <div>
+          {/* Comment */}
+          {this.state.comments.map(c => {
+            return <div>{c}</div>
+          }
+
+          )}
+        </div>
       </div>
     );
   }
